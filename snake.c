73aa5b7551ce7f3b0ed58ed84696b3d533222258
Emad Anwer <emadanwer.official@gmail.com>
2023-06-13 22:09:34 +0200

    palying area

--- snake.c
+++ snake.c
@@ -2,11 +2,18 @@
 
 int main()
 {
+	char game_area[COLUMNS][ROWS + 1];
+	int i, j;
+	initialize_game_area(game_area);
 	while (1)
 	{
 		system("clear");
 		header(1, 1);
-		game_over();
+		for (i = 0; i < COLUMNS; i++)
+			for (j = 0; j < ROWS; j++)
+				printf("%c", game_area[i][j]);
+
+		footer(IN_GAME);
 		sleep(1);
 	}
 	return (0);
--- snake.h
+++ snake.h
@@ -1,11 +1,42 @@
 #ifndef SNAKE_H
 #define SNAKE_H
-
+#define ROWS 44
+#define COLUMNS 15
 #define LINE "******************************************\n"
+#define IN_GAME 1
+#define GAME_OVER 2
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct position - place in x, y
+ * @x: x axis
+ * @y: y axis
+ */
+typedef struct position
+{
+	int x;
+	int y;
+} position_t;
+
+
+/**
+ * struct position - place in x, y
+ * @head: snake head
+ * @tail: snake tail
+ * @tall: snake tall
+ */
+typedef struct snake
+{
+	position_t head;
+	position_t tail;
+	position_t next_tail;
+	int tall;
+} snake_t;
+
 void header(int points, int level);
-void game_over();
+void playing_area(char **game_area, snake_t snake, position_t food);
+void initialize_game_area(char (*arr)[ROWS + 1]);
+void footer(int mode);
 #endif
