397bc3b573a3efb719c64952e3eeafc00255fa8e
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-06-13 20:07:19 +0300

    function/header

---
+++ header.c
@@ -0,0 +1,8 @@
+#include "snake.h"
+
+void header(int points, int level)
+{
+	printf("******************************************\n");
+	printf("*  points: %d                   level: %d  *\n", points, level);
+	printf("******************************************\n");
+}
---
+++ snake.c
@@ -0,0 +1,12 @@
+#include "snake.h"
+
+int main()
+{
+	while (1)
+	{
+		system("clear");
+		header(1, 1);
+		sleep(1);
+	}
+	return (0);
+}
